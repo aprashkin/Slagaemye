bb9e7e77ba7e03373070e18a9e8a4b1d63e6c9d0
aprAshkin <aprAshkin@DESKTOP-V1QV72U>
2023-09-29 23:32:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите целое положительное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Все возможные слагаемые числа {number}:");
+
+        FindCombinations(number, "");
+    }
+
+    static void FindCombinations(int target, string currentCombination)
+    {
+        // цикл для перебора всех возможных слагаемых от 1 до таргет
+        if (target == 0)
+        {
+            
+            Console.WriteLine(currentCombination.TrimEnd('+'));
+            return;
+        }
+        // удаляется последний символ и выводится в виде суммы как комбинация
+        for (int i = 1; i <= target; i++)
+        {
+            FindCombinations(target - i, currentCombination + i + "+");
+        }
+    }
+}
+// а вообще я битмейкер
